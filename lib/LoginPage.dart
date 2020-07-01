1ebd5f1c137e11fb3c0ac24b463f199189a03db5
gurum77 <hileejaeho@gmail.com>
2020-07-02 06:58:43 +0900

    login 만드는중

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,79 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _LoginPageState();
-  }
-}
-
-class _LoginPageState extends State<LoginPage> {
-  bool _isLoggedIn = false;
-
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  _login() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    final FirebaseUser user =
-        (await _auth.signInWithCredential(credential)).user;
-    print("signed in " + user.displayName);
-    return user;
-  }
-
-
-
-  _logout() {
-    _googleSignIn.signOut();
-    setState(() {
-      _isLoggedIn = false;
-    });
-  }
-
+class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: _isLoggedIn
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.network(
-                        _googleSignIn.currentUser.photoUrl,
-                        height: 50.0,
-                        width: 50.0,
-                      ),
-                      Text(_googleSignIn.currentUser.displayName),
-                      OutlineButton(
-                        child: Text("Logout"),
-                        onPressed: () {
-                          _logout();
-                        },
-                      )
-                    ],
-                  )
-                : Center(
-                    child: OutlineButton(
-                      child: Text("Login with Google"),
-                      onPressed: () {
-                        _login();
-                      },
-                    ),
-                  )),
-      ),
+    return Scaffold(
+      body: Center(child: Text('Login'),),
     );
   }
+
 }
